f29b60b6cab666ea9d45c768de73acb600deba83
Faba-Traore <149728585+Faba-Traore@users.noreply.github.com>
2024-05-21 20:52:06 +0100

    ..

--- js/loading.js
+++ js/loading.js
@@ -1,15 +1,39 @@
-let observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        console.log(entry)
+// let observer = new IntersectionObserver((entries)=>{
+//     entries.forEach((entry)=>{
+//         console.log(entry)
 
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        }else{
-            entry.target.classList.add('hidden');
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show')
+//         }else{
+//             entry.target.classList.add('hidden');
+//         }
+//     })
+// })
+
+
+// const hiddenElement = document.querySelectorAll('#hidden');
+// hiddenElement.forEach((el) => observer.observe(el))
+
+
+        window.addEventListener("scroll", myFunction);
+
+        function myFunction() {
+            var pageTop = document.documentElement.scrollTop;
+            var pageBottom = pageTop + window.innerHeight;
+            var tags = document.querySelectorAll("#hidden");
+
+            for (var i = 0; i < tags.length; i++) {
+                var tag = tags[i];
+                var tagTop = tag.offsetTop;
+
+                if (tagTop < pageBottom) {
+                    tag.classList.add("visible");
+                } else {
+                    tag.classList.remove("visible");
+                }
+            }
         }
-    })
-})
 
-
-const hiddenElement = document.querySelectorAll('#hidden');
-hiddenElement.forEach((el) => observer.observe(el))
+        function backToTop(){
+            window.scrollTo(0,0);
+            }
